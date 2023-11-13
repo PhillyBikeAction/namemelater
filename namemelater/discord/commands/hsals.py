f94e82b1a124a83bf78401246ab8eaf38992f96f
Ee Durbin <ewdurbin@gmail.com>
2023-11-12 19:48:24 -0500

    change commands to make one not ephemeral

--- namemelater/discord/commands/hsals.py
+++ namemelater/discord/commands/hsals.py
@@ -3,5 +3,4 @@ from discord import Interaction, app_commands
 
 @app_commands.command(name="hsals")
 async def hsals(interaction: Interaction, number: int, string: str):
-    print("im workin")
-    await interaction.response.send_message(f"{number=} {string=}", ephemeral=True)
+    await interaction.response.send_message(f"{number=} {string=}")
--- namemelater/discord/commands/slash.py
+++ namemelater/discord/commands/slash.py
@@ -3,5 +3,4 @@ from discord import Interaction, app_commands
 
 @app_commands.command(name="slash")
 async def slash(interaction: Interaction, number: int, string: str):
-    print("im workin")
     await interaction.response.send_message(f"{number=} {string=}", ephemeral=True)
